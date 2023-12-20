f9f95a1e663d3e00ced573d1daec43c2a948c080
Thecodingyogi <cynthiatonui@gmail.com>
2023-12-20 16:48:42 +0300

    Made changes to my server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripeInstance = stripe(stripeSecretKey);
 
 app.post("/create-checkout-session", async (req, res) => {
-  const MY_DOMAIN = "https://malias-bag.vercel.app/";
+  const MY_DOMAIN = "https://malias-bag-server.vercel.app/";
   const MAIN_DOMAIN = "https://malias-bag.vercel.app/";
 
   // Retrieve total amount and items from the request body
@@ -51,7 +51,7 @@ app.post("/create-checkout-session", async (req, res) => {
       },
     ],
     mode: "payment",
-    success_url: "https://malias-bag.vercel.app/Success",
+    success_url: `${MAIN_DOMAIN}/Success`,
     cancel_url: `${MAIN_DOMAIN}/Cart`,
   });
 
